6551faeb9ec8f7600e0d129270d4d21c06feb8ec
unknown <inzamam.cu@gmail.com>
2022-02-13 20:24:25 +0800

    pushing

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
+const { MongoClient } = require("mongodb");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const ObjectId = require("mongodb").ObjectID;
+// const ObjectId = require("mongodb").ObjectID;
 require("dotenv").config();
 
 const app = express();
@@ -10,36 +10,66 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const port = process.env.PORT || 5050;
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gbf8e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
 client.connect((err) => {
-  console.log("database connected");
-  const userCollection = client.db("eRMG").collection("users");
-  const materialPartsCollection = client
-    .db("eRMG")
-    .collection("materialsParts");
-  const RFQCollection = client.db("eRMG").collection("totalRFQ");
-
-  app.post("/addUser", (req, res) => {
-    const newUser = req.body;
-    console.log(newUser);
-    userCollection.insertOne(newUser).then((result) => {
-      res.send(result.insertedCount > 0);
+  console.log(err);
+  const collection = client.db("AshaIT").collection("services");
+  // console.log(collection);
+  app.get("/services", (req, res) => {
+    collection.find({}).toArray((err, service) => {
+      res.send(service);
     });
   });
-
-  app.post("/getUser", (req, res) => {
-    const newUser = req.body;
-    userCollection.find().then((result) => {
-      res.send(result);
-    });
-  });
+  // client.close();
 });
+
+// client.connect((err) => {
+//   console.log(uri);
+//   const userCollection = client.db("AshaIT").collection("users");
+//   // const adminCollection = client.db("heroRider").collection("admin");
+//   const adminCollection = client.db("AshaIT").collection("admins");
+//   const serviceCollection = client.db("AshaIT").collection("services");
+
+//   app.post("/addUser", (req, res) => {
+//     const newUser = req.body;
+//     console.log(newUser);
+//     userCollection.insertOne(newUser).then((result) => {
+//       res.send(result.insertedCount > 0);
+//     });
+//   });
+//   app.get("/services", (req, res) => {
+//     // serviceCollection.find({}).toArray((err, service) => {
+//     //   res.send(service);
+//     // });
+//     res.send("hello");
+//   });
+
+//   app.post("/addAdmin", (req, res) => {
+//     const newAdmin = req.body;
+//     adminCollection.insertOne(newAdmin).then((result) => {
+//       res.send(result.insertedCount > 0);
+//     });
+//   });
+
+//   app.get("/isAdmin/:email", (req, res) => {
+//     adminCollection.find({ email: req.params.email }).toArray((err, docs) => {
+//       res.send(docs.length > 0);
+//       console.log(docs);
+//     });
+//   });
+
+//   app.get("/getUser", (req, res) => {
+//     console.log("request is comming");
+//     userCollection.find().then((result) => {
+//       res.send(result);
+//     });
+//   });
+// });
 
 app.get("/", (req, res) => {
   res.send("Hero Rider Server Is Running!");
